47ba454ad736c879331aecc2baf57882e2deb6d1
Victor Gabriel Hilario <victorgabrielhilario28@gmail.com>
2025-07-18 00:16:53 -0300

    Java Heranca 4 - Retangulo Completo

---
+++ Heranca/retangulo_completo/Retangulo.java
@@ -0,0 +1,42 @@
+public class Retangulo extends FormaGeometrica {
+
+    public Retangulo(){
+        super();
+    }
+    private double largura;
+    private double altura;
+
+    @Override
+    public double area(){
+        return this.largura * this.altura;
+    }
+
+    public String toString(){
+        return String.format("[%s] %.2f / %.2f",Retangulo.class.getSimpleName(),  largura ,altura);
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        if (largura > 0) {
+            this.largura = largura;
+        } else {
+            throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
+        }
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if (altura > 0) {
+            this.altura = altura;
+        } else {
+            throw new IllegalArgumentException("Altura deve ser maior ou igual a 0");
+        }
+    }
+
+}

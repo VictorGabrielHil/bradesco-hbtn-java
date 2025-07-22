296d8e77e470380420b53f3185ca0de36f8b4b73
Victor Gabriel Hilario <victorgabrielhilario28@gmail.com>
2025-07-21 21:07:09 -0300

    Java Heranca - Pedido Livraria

---
+++ Heranca/pedido_livraria/Pedido.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Pedido {
+
+    private double percentualDesconto;
+    ItemPedido[] itens;
+
+    public Pedido(double percentualDesconto, ItemPedido[] itens) {
+        this.percentualDesconto = percentualDesconto / 100;
+        this.itens = itens;
+    }
+
+    public double calcularTotal(){
+        double somaTotal = 0;
+        for(ItemPedido item : itens){
+             somaTotal += (item.getQuantidade() * item.getProduto().obterPrecoLiquido());
+        }
+        double percentual= somaTotal * this.percentualDesconto;
+        return  somaTotal -percentual;
+    }
+
+
+}
---
+++ Heranca/pedido_livraria/produtos/Produto.java
@@ -0,0 +1,19 @@
+package produtos;
+
+public abstract class Produto {
+    String titulo;
+    int ano;
+    String pais;
+    double precoDouble;
+
+    public Produto(String titulo, int ano, String pais, double precoDouble) {
+        this.titulo = titulo;
+        this.ano = ano;
+        this.pais = pais;
+        this.precoDouble = precoDouble;
+    }
+
+    public abstract double obterPrecoLiquido();
+
+
+}

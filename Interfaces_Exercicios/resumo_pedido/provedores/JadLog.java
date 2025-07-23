a9ea003f9e2e9c0c1c21004d80189b670b9684d0
Victor Gabriel Hilario <victorgabrielhilario28@gmail.com>
2025-07-23 00:18:44 -0300

    Business Logic

--- Interfaces_Exercicios/resumo_pedido/provedores/JadLog.java
+++ Interfaces_Exercicios/resumo_pedido/provedores/JadLog.java
@@ -4,7 +4,7 @@ package provedores;
 public class JadLog implements  ProvedorFrete{
     @Override
     public Frete calcularFrete(double peso, double valor) {
-        double valorFrete =  peso > 7000 ? (valor * 0.2) : (valor * 0.045);
+        double valorFrete =  peso > 2000 ? (valor * 0.07) : (valor * 0.045);
         return new Frete(valorFrete, obterTipoProvedorFrete());
     }
 

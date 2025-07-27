9b9bcfbadb215da2a9a3e69012624817bfe07f00
Victor Gabriel Hilario <victorgabrielhilario28@gmail.com>
2025-07-27 13:27:18 -0300

    Sem /n

--- 0x02/divisao/Numero.java
+++ 0x02/divisao/Numero.java
@@ -3,12 +3,11 @@ public class Numero {
     public static void dividir(int a, int b){
 
         try{
-             c = a /b;
-
+             c = a / b;
         }catch (ArithmeticException e){
             System.out.println("Nao eh possivel dividir por zero");
         } finally {
-            System.out.printf("%d / %d = %d\n", a, b, c);
+            System.out.printf("%d / %d = %d", a, b, c);
         }
     }
 

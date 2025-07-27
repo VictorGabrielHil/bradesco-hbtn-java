f48193344b74f64abfc75cc7cc1da55f8c722ad6
Victor Gabriel Hilario <victorgabrielhilario28@gmail.com>
2025-07-27 13:23:52 -0300

    Java Execption - Divisao

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,15 @@
+public class Numero {
+    static int c;
+    public static void dividir(int a, int b){
+
+        try{
+             c = a /b;
+
+        }catch (ArithmeticException e){
+            System.out.println("Nao eh possivel dividir por zero");
+        } finally {
+            System.out.printf("%d / %d = %d\n", a, b, c);
+        }
+    }
+
+}

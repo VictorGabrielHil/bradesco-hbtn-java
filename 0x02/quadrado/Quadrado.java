b79336d62de5b7b170b3252726b366ff889f0f7e
Victor Gabriel Hilario <victorgabrielhilario28@gmail.com>
2025-09-03 22:04:11 -0300

    Java Exception - Quadrado

--- 0x02/quadrado/Quadrado.java
+++ 0x02/quadrado/Quadrado.java
@@ -2,7 +2,7 @@ public class Quadrado {
     public static double area(double lado){
 
         if (lado > 0) {
-            return lado;
+            return Math.pow(lado,2);
         }
         else {
             throw new IllegalArgumentException("Lado deve possuir valor positivo");
